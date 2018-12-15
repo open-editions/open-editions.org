d5be2bdd04a03659124040bfe353510f44d44710
Jonathan Reeve <jon.reeve@gmail.com>
2018-12-15 12:29:16 -0500

    add blaze and clay as dependencies

--- open-editions.nix
+++ open-editions.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, hakyll, stdenv }:
+{ mkDerivation, base, blaze-html, clay, hakyll, stdenv }:
 mkDerivation {
   pname = "open-editions";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base hakyll ];
+  executableHaskellDepends = [ base blaze-html clay hakyll ];
   license = "unknown";
   hydraPlatforms = stdenv.lib.platforms.none;
 }
