9e70a44a23d1c65231dad46ef6a8bd9fd89d39aa
Sridhar Ratnakumar <srid@srid.ca>
2020-01-22 11:05:12 -0500

    Use gitignoreSource, like rib-sample

--- default.nix
+++ default.nix
@@ -1,10 +1,12 @@
 let
   ribRevision = "a57e7ab7ef5b5b5fcbbf7b99b3edb33bfde5851f";
+
+  inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
 # Rib library source to use
   rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
 # Cabal project root
-, root ? ./.
+, root ? gitignoreSource ./.
 # Cabal project name
 , name ? "open-editions-org"
 , ...
