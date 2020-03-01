9154a82eda7405d894263b8527a6b476a9034209
Jonathan Reeve <jon.reeve@gmail.com>
2020-03-01 13:14:28 -0500

    fill out XML to HTML transformation a little more

--- XML2HTML.hs
+++ XML2HTML.hs
@@ -3,43 +3,44 @@
 
 module Main where
 
+import           Clay                          hiding (transform, type_)
 import           Control.Monad                        (join)
--- import           Control.Monad.Trans.Class            (lift)
 import           Control.Applicative                  ((<$>))
--- import           Controllers.Home                     (home, login, post)
-import qualified Data.Map as M
+import qualified Data.Map.Strict as M
 import           Data.Maybe                           (fromMaybe)
 import           Data.Text                            (Text, concat)
--- import           Network.Wai.Middleware.RequestLogger (logStdoutDev)
--- import           Network.Wai.Middleware.Static        (addBase, noDots,
-                                                       -- staticPolicy, (>->))
+import qualified Data.Text.IO as TIO
+import qualified Data.Text.Lazy as TL
+import           Lucid
 import           Prelude
 import           System.Environment                   (lookupEnv)
--- import           Text.Blaze.Html5                     (Html, pre, toHtml)
 import           Text.Read                            (readMaybe)
 import           Text.XML
 import           Text.XML.Cursor
--- import           Web.Scotty                           (middleware, scotty)
 
 main :: IO ()
 main = do
-  doc <- Text.XML.readFile def "test-data/header.xml"
-  let cursor = fromDocument doc
-      filenames = cursor $// laxElement "include" >=> attribute "href"
-  print filenames
+  -- Extract child files from header.xml
+  -- doc <- Text.XML.readFile def "test-data/header.xml"
+  -- let cursor = fromDocument doc
+  --     filenames = cursor $// laxElement "include" >=> attribute "href"
+  -- print filenames
 
   -- TODO: load and parse all child files
 
-  Document prologue root epilogue <- Text.XML.readFile def "test-data/3020.xml"
+  Document prologue root epilogue <- Text.XML.readFile def "test-data/01_sis.xml"
   let root' = transform root
-      asText = renderText def (Document prologue root' epilogue)
-      rendered = asText -- toHtml asText
-
-  print rendered
+      -- Make an empty prologue
+      prologue' = Prologue [] Nothing []
+      -- Make an empty epilogue
+      epilogue' = []
+      rendered = Text.XML.renderText def (Document prologue' root' epilogue')
+      html = makeHtml (TL.toStrict rendered)
+  Lucid.renderToFile "out.html" html
 
 transform :: Element -> Element
 transform (Element _name attrs children) =
-  Element "html" M.empty $ concatMap goNode children
+  Element "div" (M.fromList [("class"::Name,"container")]) $ concatMap goNode children
 
 goNode :: Node -> [Node]
 goNode (NodeElement e) = [NodeElement $ goElem e]
@@ -54,7 +55,7 @@ goElem (Element name attrs children) =
     -- Comment out metadata in header
     "teiHeader" -> Element "div" (hidden attrs) transformedChildren
     "lg" -> Element "div" lgAttrs transformedChildren
-    "l" -> Element "span" M.empty transformedChildren
+    "l" -> Element "span" (M.fromList [("class"::Name, "line")]) transformedChildren
     "head" -> Element "h1" M.empty transformedChildren
     "lb" -> Element "span" (lbAttrs attrs) (lbChildren attrs)
     "said" -> Element "span" (saidAttrs attrs) (saidChildren attrs children)
@@ -64,6 +65,7 @@ goElem (Element name attrs children) =
     transformedChildren = concatMap goNode children
     hidden mattrs = M.insert "class" "hidden" mattrs
     lgAttrs = M.delete "xmlns" $ M.fromList [("class" :: Name, "stanza")]
+    -- TODO: convert this line number
     lbAttrs attrs = M.fromList [("class" :: Name, "lineNum")]
     lbChildren attrs = [NodeContent (attrs M.! "n")]
     saidAttrs attrs = setClass "dialogueAttribution"
@@ -85,3 +87,28 @@ setClass :: Text -> M.Map Name Text
 setClass val = mkAttrs "class" val
 
 spanIt name attrs children = Element "span" (M.insert "class" name attrs) children
+
+makeHtml :: Text -> Html ()
+makeHtml contents = html_ [] $ do
+  head_ [] $ do
+    style_ [type_ "text/css"] $ Clay.render pageStyle
+  body_ [] $ do
+    div_ [id_ "lMargin"] ""
+    (toHtmlRaw contents)
+    div_ [id_ "rMargin"] ""
+
+pageStyle :: Css
+pageStyle = do
+  "body" ? do
+    display flex
+    flexDirection row
+    justifyContent center
+  -- "div.container, #lMargin, #rMargin" ? display flex
+  "span.line" ? display block
+  "span.lineNum" ? do
+    display block
+    position relative
+    right (px 110)
+  "div.stanza" ? (margin (em 1) (em 1) 0 0)
+  "img" ? do
+    maxWidth (pct 100)
