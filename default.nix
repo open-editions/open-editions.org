e42c3b3289caaf39613680f1105efc9cfd89a680
Jonathan Reeve <jon.reeve@gmail.com>
2020-03-01 11:35:01 -0500

    whitespace changes

--- default.nix
+++ default.nix
@@ -1,15 +1,14 @@
 let
   ribRevision = "e41eae3";
-
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
-# Rib library source to use
+  # Rib library source to use
   rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
-# Cabal project root
-, root ? gitignoreSource ./.
-# Cabal project name
-, name ? "open-editions-org"
-, ...
+  # Cabal project root
+  , root ? gitignoreSource ./.
+  # Cabal project name
+  , name ? "open-editions-org"
+  , ...
 }:
 
 import rib { inherit root name; }
