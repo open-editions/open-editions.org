2664a50c699c7cafefc5bb5f58a7f460e4b0150b
Jonathan Reeve <jon.reeve@gmail.com>
2022-10-24 17:15:46 -0400

    commit uncommitted stuff

---
+++ criticism-analysis/do-matching.py
@@ -0,0 +1,7 @@
+import text_matcher
+
+text1 = "texts/corpus-joyce-finnegans-wake-tei/finnegans-wake-corrected.xml"
+
+text2 = "/home/jon/Code/corpus-joyce-finnegans-wake-tei/criticism-analysis/1/ocr"
+
+text_matcher.text_matcher.cli(text1, text2)
